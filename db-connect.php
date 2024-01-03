2dfada053ebff790a8ab53a6328f409312213a30
GeorgStrassberger <georg.strassberger@googlemail.com>
2024-01-03 10:51:52 +0100

    changed connecten data

--- db-connect.php
+++ db-connect.php
@@ -1,7 +1,7 @@
 <?php
-
+require_once(__DIR__ . '/env.php');
 try {
-    $pdo = new PDO('mysql:host=localhost; dbname=php_cities', 'php_cities', 'vnW3azxJL1Go8cdZ', [
+    $pdo = new PDO("mysql:host=localhost; dbname=$dbname", "$user", "$pw", [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 }
